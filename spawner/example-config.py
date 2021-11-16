17827769d678ec0f7f0a055440d7477784c09f98
Fengrui Lu (WICRESOFT NORTH AMERICA LTD) <v-fengruilu@microsoft.com>
2021-11-16 15:34:50 +0800

    upgrade servicebus sdk from T1 to T2

--- spawner/example-config.py
+++ spawner/example-config.py
@@ -5,7 +5,9 @@ class AzureContext(object):
 
       remarks:
       This is a helper to combine service principal credentials with the subscription id.
-      See README for information on how to obtain a service principal attributes client id, secret, etc. for Azure
+      Notice that the client is using default Azure credentials.
+      To make default credentials work, ensure that environment variables 'Azure_Client_ID',
+      'AZURE_CLIENT_SECRET' and 'AZURE_TENANT_ID' are set with the service principal credentials.
    """
    
    def __init__(self, subscription_id):
